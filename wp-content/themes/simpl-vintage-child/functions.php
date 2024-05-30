d84db9f44921c5cb916c4de695f42ff60871b322
Pierre Bultez <pierre.bultez@proton.me>
2024-05-30 20:50:00 +0200

    modification functions.php -> // Empêche l'accès direct + code thème enfant

--- wp-content/themes/simpl-vintage-child/functions.php
+++ wp-content/themes/simpl-vintage-child/functions.php
@@ -1,6 +1,23 @@
 <?php
-function my_theme_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+// function my_theme_enqueue_styles() {
+//     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+// }
+// add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+//** Activation du thème enfant **//
+
+// Empêche l'accès direct
+if (!defined('ABSPATH')) {
+    exit; // Quitte si accès direct
 }
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+// Enqueue les styles et scripts du thème parent et du thème enfant
+function enqueue_parent_child_styles() {
+    // Enqueue le style du thème parent
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+    // Enqueue le style du thème enfant
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version'));
+}
+add_action('wp_enqueue_scripts', 'enqueue_parent_child_styles');
 ?>
