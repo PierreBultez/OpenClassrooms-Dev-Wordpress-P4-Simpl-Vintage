5bb0ff3a98481316fd7ee5b1304d3169db6e6adb
Pierre Bultez <pierre.bultez@proton.me>
2024-05-30 20:46:13 +0200

    site terminé

---
+++ wp-content/themes/simpl-vintage-child/single-layout.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template for Single post
+ *
+ * @package     Astra
+ * @author      Astra
+ * @copyright   Copyright (c) 2020, Astra
+ * @link        https://wpastra.com/
+ * @since       Astra 1.0.0
+ */
+
+?>
+
+<div <?php astra_blog_layout_class( 'single-layout-1' ); ?>>
+
+
+				echo astra_attr(
+					'article-entry-content-single-layout',
+					array(
+						'class' => '',
+					)
+				);
+				?>
+	>
+
+		<?php astra_entry_content_before(); ?>
+
+		<?php the_content(); ?>
+
+		<?php
+			astra_edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					esc_html__( 'Edit %s', 'astra' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+
+		<?php astra_entry_content_after(); ?>
+
+
+	</div><!-- .entry-content .clear -->
+</div>
